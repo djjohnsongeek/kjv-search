41abd8380a1100d175b4cdbaf0e3ba0f26d75bed
djjohnsongeek <danieleejohnson@gmail.com>
2020-09-04 12:20:09 -0400

    improved generatepretty_ref() added generate_search_results_html()

--- util/functions.php
+++ util/functions.php
@@ -1,5 +1,5 @@
 <?PHP
-// A big file of functions
+# A big file of functions
 require_once("config.php");
 
 function search($db, $query, $page) {
@@ -218,8 +218,84 @@ function get_verse_number($ref) {
 
 function generate_pretty_ref($ref){
     $referance = ucfirst($ref["book"])
-               . " " . $ref["chapter"]
-               . ":" . $ref["verse"];
+               . " "
+               . $ref["chapter"];
 
+    if ($ref["verse"]) {
+        $referance .= ":" . $ref["verse"];
+    }
+               
     return $referance;
+}
+
+function generate_search_results_html($page, $query, $count, $search_results) {
+    $html = "";
+    if($query && $count > 0 && !isset($search_results["display"])) {
+
+
+        // paginaton links
+        $prev_link = "<a href='http://biblesearch/?q=" . $query . "&p=" . strval($page - 1) . "'>Prev</a> | ";
+        $next_link = "<a href='http://biblesearch/?q=" . $query . "&p=" . strval($page + 1) . "'>Next</a><br/>";
+
+        $prev_link = $page >= 1 ? $prev_link : "<span class='disabled'>Prev</span> | ";
+        $next_link = $count > 10 ? $next_link : "<span class='disabled'>Next</span><br/>";
+
+        $html .= $prev_link . $next_link;
+
+        // metrics
+        $html .= "<span class='metrics'>" . $count . " results in " . $search_results["time"] . " seconds.</span><br/>";
+    }
+
+    // render block of verses
+    if(isset($search_results["display"])) {
+        //debug_print(array($search_results));
+        $html .= "<span class='metrics'>"
+                . "Result found in "
+                . $search_results["time"]
+                . " seconds.</span><br/>";
+
+        $html .= "<div class='verse-container'>";
+        $html .= "<div class='referance'><strong>" . $search_results["gen-referance"] . "</strong></div>";
+        $html .= "<div>";
+
+        foreach($search_results as $result) {
+            if (isset($result["reference"])) {
+                $verse_html = "<span class='verse-number'>"
+                            . $result["verse_id"]
+                            . "</span>"
+                            . " " . $result["verse"] . " ";
+                $html .= $verse_html;
+            }
+        }
+
+        $html .= "</div></div>";        
+    }
+    // render rows of verses
+    else {
+        $render_count = 0;
+        foreach($search_results as $result) {
+            // don't render the 11th result
+            if ($render_count === 10) {break;}
+
+            // only render results with a "reference"
+            if (isset($result["reference"])) {
+                $verse = emphasize_result($query, $result["verse"]);
+        
+                // render result
+                $html .= "
+                <div class='verse-container'>
+                    <div class='referance'><strong >" . $result["reference"] . "</strong></div>
+                    <div>" . $verse . "</div>
+                </div>";
+                $render_count++;
+            }
+        }
+    }
+
+    // No results message
+    if ($count === 0) {
+        $html .= "<div class='no-results-message'><span>No Results Found</span></div>";
+    }
+
+    return $html;
 }
