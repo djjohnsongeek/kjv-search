9ae99c9cff97700707dc9c72e96fdcb9489ddac5
djjohnsongeek <danieleejohnson@gmail.com>
2020-09-05 06:39:01 -0400

    instantiate DB object

--- util/init.php
+++ util/init.php
@@ -2,10 +2,6 @@
 require_once("config.php");
 require_once("functions.php");
 require_once("validations.php");
+require_once("Database.php");
 
-// set up db connection
-$db = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT);
-if ($db->connect_errno) {
-    exit("SQL Connection Error - {$db->connect_errorno}: {$db->connect_error}");
-}
-$db->set_charset('utf8');
+$db = new Database();
