7be354db1509cb6ff6a44ffdeda609ab043d1348
djjohnsongeek <danieleejohnson@gmail.com>
2020-09-09 14:58:30 -0400

    update init require path

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?PHP
 #### Gathers the user's search query and renders the results ####
-require_once("util/init.php");
+require_once("src/init.php");
 
 $query = $_GET["q"] ?? NULL;
 $page = $_GET["p"] ?? 0;
+
 if (!validate_page($page)) {
     redirect(SITE_URL);
 }
@@ -12,5 +13,6 @@ $page = intval($page);
 $search_results = search($db, $query, $page);
 
 $db->close();
+
 // render page
 include_once(DIR_TEMPLATES . "main.php");
