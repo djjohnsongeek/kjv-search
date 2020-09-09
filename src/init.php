de75157a1287db96a5ec51f03c478752a4712cb2
djjohnsongeek <danieleejohnson@gmail.com>
2020-09-09 15:06:10 -0400

    updated paths

---
+++ src/init.php
@@ -0,0 +1,8 @@
+<?PHP
+require_once("config/config.php");
+require_once(DIR_SRC . "render.php");
+require_once(DIR_SRC . "search.php");
+require_once(DIR_SRC . "functions.php");
+require_once(DIR_CLASSES .  "Database.php");
+
+$db = new Database();
