8e40a5d9babd43ba760c87c98b1f8ebd9b147503
djjohnsongeek <danieleejohnson@gmail.com>
2020-09-11 06:47:36 -0400

    renamed functions.php to util.php

--- src/init.php
+++ src/init.php
@@ -1,8 +1,9 @@
-<?PHP
+<?php
+
 require_once("config/config.php");
 require_once(DIR_SRC . "render.php");
 require_once(DIR_SRC . "search.php");
-require_once(DIR_SRC . "functions.php");
+require_once(DIR_SRC . "util.php");
 require_once(DIR_CLASSES .  "Database.php");
 
 $db = new Database();
